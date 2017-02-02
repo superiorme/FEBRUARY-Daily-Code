d1e7d9fca9552ac31688792e505f01d8d019b8e2
superiorme <634206@dpsk12.net>
2017-02-02 08:21:48 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+
+int main() {
+
+	int input;
+	int num;
+
+
+	cout << "what song do you what to listen 1,2,3,4" << endl;
+	cin >> input;
+
+	if (input<1)
+		cout << "all around me familiar faces worn out places " << endl;
+	else if (input<2)
+		cout << "Every step that I take is another mistake to you Caught in the undertow, just caught in the undertow" << endl;
+	else if (input<3)
+		cout << "Despacito Mami yo sé que te va a gustar Despacito Mami yo sé que te va a encantar Mueve dale vamo' Despacito Mami yo sé que te va a gustar Mueve dale vamo' Sabrosito Vamo' a bailar" << endl;
+	else (input<4);
+		cout << "I've got no strings To hold me down To make me fret Or make me frown I had strings But now I'm free There are no strings on me" << endl;
+
+}
