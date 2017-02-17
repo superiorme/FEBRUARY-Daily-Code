a0433cf5076b74e4dc13aa2dfea0db82e120d6fb
superiorme <634206@dpsk12.net>
2017-02-17 08:05:49 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+	char input = 'y';
+
+	int y;
+	int n;
+		
+	while (input > 100); {
+		
+
+		cout <<"would you like a cookie"<<endl;
+		cin >> input;
+
+		if (input == 'y')
+			cout << "here's a cookie"<< endl;
+		
+
+		else if (input == 'n')
+			cout <<"thank you for eating cookies"<< endl;
+		return 0;
+		
+}
+
+	}
+
