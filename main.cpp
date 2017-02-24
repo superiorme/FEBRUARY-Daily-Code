8be2d9f6c895852f463c8d5d09ba0e294c93f325
superiorme <634206@dpsk12.net>
2017-02-24 07:47:15 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+
+using namespace std;
+
+
+int main() {
+
+	double number = 1;
+
+	for (int i = 0; i < 11; i++) {
+
+		number = number * 10;
+		cout << number << endl;
+	}
+}
