931d23a3bbac751f325ccaec2d29627450caf2e2
superiorme <634206@dpsk12.net>
2017-02-28 08:14:21 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,49 @@
+#include <Windows.h>//Made by TJ and Marcio
+void Annoying();
+int main() {
+	Annoying();
+}
+void Annoying() {
+	for (int i = 0; i < 250; i++) {
+		Beep(1000, 100);
+		system("Color A5");
+		MessageBox(NULL, "teach me", "doctor strange", NULL);
+		Beep(1000, 100);
+		system("Color 1A");
+		//MessageBox(NULL, "Backslash!!", "A vision!", NULL);
+		Beep(1000, 100);
+		system("Color 2B");
+		//MessageBox(NULL, "Backslash!!", "A vision!", NULL);
+		Beep(1000, 100);
+		system("Color 3C");
+		//MessageBox(NULL, "Backslash!!", "A vision!", NULL);
+		Beep(1000, 100);
+		system("Color 4D");
+		MessageBox(NULL, "Backslash!!", "A vision!", NULL);
+		Beep(1000, 100);
+		system("Color 5E");
+		//MessageBox(NULL, "Backslash!!", "A vision!", NULL);
+		Beep(1000, 100);
+		system("Color 6F");
+		//MessageBox(NULL, "Backslash!!", "A vision!", NULL);
+		Beep(1000, 100);
+		system("Color A1");
+		//MessageBox(NULL, "Backslash!!", "A vision!", NULL);
+		Beep(1000, 100);
+		system("Color B2");
+		//MessageBox(NULL, "Backslash!!", "A vision!", NULL);
+		Beep(1000, 100);
+		system("Color C3");
+		//MessageBox(NULL, "Backslash!!", "A vision!", NULL);
+		Beep(1000, 100);
+		system("Color D4");
+		MessageBox(NULL, "Do you really want to keep going?", "STOP", NULL);
+		Beep(1000, 100);
+		system("Color E5");
+		//MessageBox(NULL, "Backslash!!", "A vision!", NULL);
+		Beep(1000, 100);
+		system("Color F6");
+		//MessageBox(NULL, "Backslash!!", "A vision!", NULL);
+		Beep(1000, 100);
+	}
+}
