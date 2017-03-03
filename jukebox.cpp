9b7437dd82cab5bf7194bcf7817c1c0934fadc94
superiorme <634206@dpsk12.net>
2017-03-03 08:59:36 -0700

    Rename main.cpp to jukebox.cpp

--- jukebox.cpp
+++ jukebox.cpp
@@ -18,7 +18,7 @@ int main() {
 		cout << "Every step that I take is another mistake to you Caught in the undertow, just caught in the undertow" << endl;
 	
 	else if (input<3)
-		cout << "Despacito Mami yo sé que te va a gustar Despacito Mami yo sé que te va a encantar Mueve dale vamo' Despacito Mami yo sé que te va a gustar Mueve dale vamo' Sabrosito Vamo' a bailar" << endl;
+		cout << "Despacito Mami yo sÃ© que te va a gustar Despacito Mami yo sÃ© que te va a encantar Mueve dale vamo' Despacito Mami yo sÃ© que te va a gustar Mueve dale vamo' Sabrosito Vamo' a bailar" << endl;
 	
 	else (input<4);
 		cout << "I've got no strings To hold me down To make me fret Or make me frown I had strings But now I'm free There are no strings on me" << endl;
